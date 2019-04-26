b1c1e86a8567213aad08003f726d8485ecd635e9
magicsli <xiaoming@qq.com>
2019-04-26 11:50:30 +0800

    项目插件配置  css按需加载, 路由, redux antd库

---
+++ src/containers/register/register.jsx
@@ -0,0 +1,13 @@
+
+/* 注册路由组件 */
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+  render() {
+    return (
+      <div>
+          Register
+      </div>
+    )
+  }
+}
